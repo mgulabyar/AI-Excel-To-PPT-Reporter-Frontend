eee08f5f11d5371b8a3c1c761e0e158c118ce9b1
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-05 22:49:07 +0500

    refactor: clean up boilerplate code

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -51,7 +51,7 @@ const App: React.FC = () => {
         data_summary: dataSummary,
       });
       setReport(res.data.report);
-      setTabValue(0); // Reset 
+      setTabValue(0);
     } catch (e) {
       alert("Error connecting to Engine 8009");
     }
